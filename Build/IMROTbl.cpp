d28a18be60446c7caa3525225abeb0c4ea48e3fa
billKarsh <karshb@janelia.hhmi.org>
2024-11-14 19:39:01 -0800

    Support NP2021 quad-probe

--- Build/IMROTbl.cpp
+++ Build/IMROTbl.cpp
@@ -398,6 +398,7 @@ IMROTbl::IMROTbl( const QString &pn, int type ) : pn(pn), type(type)
                 _zpitch     = 15;
                 break;
             case 2020:  // 2.0 quad base (Ph 2C)
+            case 2021:  // 2.0 quad base (Ph 2C) with cap
                 _ncolhwr    = 2;
                 _ncolvis    = 2;
                 col2vis_ev  = {0,1};
@@ -1181,6 +1182,7 @@ bool IMROTbl::pnToType( int &type, const QString &pn )
                 supp = true;
                 break;
             case 2020:  // Neuropixels 2.0 quad base (Ph 2C)
+            case 2021:  // Neuropixels 2.0 quad base (Ph 2C) with cap
                 type = 2020;
                 supp = true;
                 break;
@@ -1281,6 +1283,7 @@ IMROTbl* IMROTbl::alloc( const QString &pn )
             case 2014:  // Neuropixels 2.0 multishank probe with cap
                 return new IMROTbl_T2013( pn );
             case 2020:  // Neuropixels 2.0 quad base (Ph 2C)
+            case 2021:  // Neuropixels 2.0 quad base (Ph 2C) with cap
                 return new IMROTbl_T2020( pn );
             case 3010:  // NXT single shank (Ph 1B)
             case 3011:  // NXT single shank (Ph 1B) with cap
